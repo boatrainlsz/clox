5384f5cf674bc9b076343e91bd9855036081391e
boatrainlsz <yinger521999@gmail.com>
2022-10-19 21:42:57 +0800

    15虚拟机栈初步完成

--- chunk.h
+++ chunk.h
@@ -6,6 +6,11 @@
 
 typedef enum {
     OP_CONSTANT,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    OP_NEGATE,
     OP_RETURN,
 } OpCode;
 
@@ -21,7 +26,7 @@ void initChunk(Chunk *chunk);
 
 void freeChunk(Chunk *chunk);
 
-void writeChunk(Chunk* chunk, uint8_t byte, int line);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
 
 int addConstant(Chunk *chunk, Value value);
 
