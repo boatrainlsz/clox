946bc24c0044094fba775677199d31ab11f260b5
boatrainlsz <yinger521999@gmail.com>
2022-10-24 21:29:58 +0800

    [chapter19]:19.3字符串的底层表示

--- compiler.h
+++ compiler.h
@@ -1,8 +1,10 @@
 #ifndef clox_compiler_h
 #define clox_compiler_h
 
+#include "object.h"
+#include "vm.h"
 #include "chunk.h"
 
-bool compile(const char* source, Chunk* chunk);
+bool compile(const char *source, Chunk *chunk);
 
 #endif
--- memory.h
+++ memory.h
@@ -3,6 +3,9 @@
 
 #include "common.h"
 
+#define ALLOCATE(type, count) \
+    (type*)reallocate(NULL, 0, sizeof(type) * (count))
+
 #define GROW_CAPACITY(capacity) \
     ((capacity) < 8 ? 8 : (capacity) * 2)
 
@@ -13,5 +16,6 @@
 #define FREE_ARRAY(type, pointer, oldCount) \
     reallocate(pointer, sizeof(type) * (oldCount), 0)
 
-void* reallocate(void* pointer, size_t oldSize, size_t newSize);
+void *reallocate(void *pointer, size_t oldSize, size_t newSize);
+
 #endif
