4b9ba9165d923d9efc6942465bd6ed4755c8b101
boatrainlsz <yinger521999@gmail.com>
2022-10-24 21:50:05 +0800

    [chapter19]:19.4字符串链接

--- value.c
+++ value.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
+#include <string.h>
 
+#include <stdio.h>
 #include "memory.h"
 #include "value.h"
+#include "object.h"
 
 void initValueArray(ValueArray *array) {
     array->values = NULL;
@@ -37,6 +39,10 @@ void printValue(Value value) {
         case VAL_NUMBER:
             printf("%g", AS_NUMBER(value));
             break;
+        case VAL_OBJ:
+            printObject(value);
+            break;
+
     }
 }
 
@@ -49,6 +55,13 @@ bool valuesEqual(Value a, Value b) {
             return true;
         case VAL_NUMBER:
             return AS_NUMBER(a) == AS_NUMBER(b);
+        case VAL_OBJ: {
+            ObjString *aString = AS_STRING(a);
+            ObjString *bString = AS_STRING(b);
+            return aString->length == bString->length &&
+                   memcmp(aString->chars, bString->chars,
+                          aString->length) == 0;
+        }
         default:
             return false; // Unreachable.
     }
